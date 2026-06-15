9102326193513377533d339b5e6eb825617ade0a
Tommy Chung <tc@tommy-chung.com>
2026-06-15 16:13:49 -0400

    feat: add new skill icons and update existing ones in Skills component

---
+++ app/components/icons/opencode.tsx
@@ -0,0 +1,22 @@
+import type { SVGProps } from 'react';
+
+const OpenCode = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		{...props}
+		xmlnsXlink="http://www.w3.org/1999/xlink"
+		viewBox="0 0 512 512"
+	>
+		<svg viewBox="0 0 512 512" fill="none">
+			<rect width="512" height="512" fill="#131010" />
+			<path d="M320 224V352H192V224H320Z" fill="#5A5858" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M384 416H128V96H384V416ZM320 160H192V352H320V160Z"
+				fill="white"
+			/>
+		</svg>
+	</svg>
+);
+
+export { OpenCode };
